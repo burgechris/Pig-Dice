1ae422c277d0d8f6309f747d73acc9e13f626277
Christopher Burge and Dominique Youmans <christopher+dominique@>
2019-08-28 15:17:10 -0700

    figure out how to switch players

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,62 @@
 // Back-end Function
-function Player(name, totalScore) {
+
+//Player Object
+function Player(name) {
   this.name = name,
-  this.totalScore = 0,
+  this.gameScore = 0,
   this.roundScore = 0
 }
-
-Player.prototype.rollDie = function() {
-  this.roundScore = Math.floor(Math.random() * 6) + 1;
+//player clicks roll dice
+//Die returns a random number between 1 and 6
+Player.prototype.rollDie = function(roll) {
+  var roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+  if (roll === 1) {
+    this.roundScore = 0;
+    Game.SwitchPlayer();
+  } else {
+    this.roundScore += roll;
+  }
 }
+//That number is added to their round score
+//player chooses to roll dice again
+//Die returns another randome number
+//New number is added to the round score
+//player has two options to end turn
+//player can click hold
+//round score is added to total score
+//turn ends
+//placer rolls a 1
+//round score goes to zero
+//turn ends
+
+//Game Object
+function Game(player1, player2) {
+  var player1 = new Player("player1");
+  var player2 = new Player("player2");
+  this.currentPlayer = [player1, player2];
+  console.log(this.currentPlayer);
+  this.maxScore = 20
+}
+  //2 players
+Game.prototype.SwitchPlayer = function() {
+  // this.currentPlayer[0], this.currentPlayer[1] = this.currentPlayer[1];
+}
+  //The game has a max score
+
+
 
 Player.prototype.hold = function() {
-  this.totalScore += this.roundScore
+  this.gameScore += this.roundScore
 }
 
+//UI function
+$(document).ready(function(){
+  $(".pigDice#roll").submit(function(event){
+    // var newPlayer = new Player("steve");
+    // var rollDice = Player.rollDie();
+    event.preventDefault();
 
-function Round() {
-  this.currentPlayer = currentPlayer
-}
+
+  });
+});
